5a93e2a79814d600c884be8b80a2084e9b1eaf11
jakariamasum <jmphy698@gmail.com>
2024-06-12 10:58:52 +0600

    booking interface and model created

---
+++ src/app/modules/booking/booking.model.ts
@@ -0,0 +1,38 @@
+import { Schema, model } from "mongoose";
+import { TBooking } from "./booking.interface";
+
+const bookingSchema = new Schema<TBooking>(
+  {
+    date: {
+      type: String,
+      required: true,
+    },
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    car: {
+      type: Schema.Types.ObjectId,
+      ref: "Car",
+      required: true,
+    },
+    startTime: { type: String, required: true },
+    endTime: {
+      type: String,
+      default: "",
+    },
+    totalCost: {
+      type: Number,
+      default: 0,
+    },
+    isBooked: {
+      type: String,
+      enum: ["unconfirmed", "confirmed"],
+      default: "unconfirmed",
+    },
+  },
+  { timestamps: true }
+);
+
+export const Booking = model<TBooking>("Booking", bookingSchema);
